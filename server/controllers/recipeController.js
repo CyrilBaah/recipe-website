3dadd18ece3db8a4a1b3cb67c46f66cc71e51f91
CyrilBaah <cyril.baah@amalitech.org>
2022-03-28 21:29:27 +0000

    fetch from data db

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -7,45 +7,50 @@ const Category = require("../models/category");
  */
 
 exports.homePage = async (req, res) => {
-  res.render("index", { title: "Recipe Website - Home" });
+  try {
+    const limitNumber = 5;
+    const categories = await Category.find({}).limit(limitNumber);
+    res.render("index", { title: "Recipe Website - Home", categories });
+  } catch (error) {
+    res.status(500).send({ message: error.message || "Error Occured" });
+  }
 };
-
 
 /**
  * Dummy Data Example
-*/
+ */
 
-async function insertDymmyCategoryData(){
+async function insertDymmyCategoryData() {
   try {
     await Category.insertMany([
       {
-        "name": "Thai",
-        "image": "thai-food.jpg"
+        name: "Thai",
+        image: "thai-food.jpg",
       },
       {
-        "name": "American",
-        "image": "american-food.jpg"
+        name: "American",
+        image: "american-food.jpg",
       },
       {
-        "name": "Chinese",
-        "image": "chinese-food.jpg"
+        name: "Chinese",
+        image: "chinese-food.jpg",
       },
       {
-        "name": "Mexican",
-        "image": "mexican-food.jpg"
+        name: "Mexican",
+        image: "mexican-food.jpg",
       },
       {
-        "name": "Indian",
-        "image": "indian-food.jpg"
+        name: "Indian",
+        image: "indian-food.jpg",
       },
       {
-        "name": "Spanish",
-        "image": "spanish-food.jpg"
-      }
+        name: "Spanish",
+        image: "spanish-food.jpg",
+      },
     ]);
   } catch (error) {
-    console.log('error', + error)
+    console.log("error", +error);
   }
 }
 
-insertDymmyCategoryData();
+// insertDymmyCategoryData();
