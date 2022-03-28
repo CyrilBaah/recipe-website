aec0fb77fe81f3f15aa94145930603401f4c76ac
CyrilBaah <cyril.baah@amalitech.org>
2022-03-28 21:22:05 +0000

    Generate data

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,8 +1,51 @@
+require("../config/database");
+const Category = require("../models/category");
+
 /**
  * GET /
  * Homepage
  */
 
 exports.homePage = async (req, res) => {
-  res.render("index", { title: 'Recipe Website - Home'});
+  res.render("index", { title: "Recipe Website - Home" });
 };
+
+
+/**
+ * Dummy Data Example
+*/
+
+async function insertDymmyCategoryData(){
+  try {
+    await Category.insertMany([
+      {
+        "name": "Thai",
+        "image": "thai-food.jpg"
+      },
+      {
+        "name": "American",
+        "image": "american-food.jpg"
+      },
+      {
+        "name": "Chinese",
+        "image": "chinese-food.jpg"
+      },
+      {
+        "name": "Mexican",
+        "image": "mexican-food.jpg"
+      },
+      {
+        "name": "Indian",
+        "image": "indian-food.jpg"
+      },
+      {
+        "name": "Spanish",
+        "image": "spanish-food.jpg"
+      }
+    ]);
+  } catch (error) {
+    console.log('error', + error)
+  }
+}
+
+insertDymmyCategoryData();
